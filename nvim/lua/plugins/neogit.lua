16878d191d17e3001274ff8ac66bd055ddeaf037
nixxer <163311591+Tradgardsmastaren@users.noreply.github.com>
2025-09-12 20:58:42 +0200

    magic keybind

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,7 +1,18 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "nvim-lua/plenary.nvim",      -- required
+    "sindrets/diffview.nvim",     -- optional
+  },
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        require('neogit').open()
+      end,
+      desc = "Open Neogit",
+    },
   },
 }
+
+
